da363a1530240166cfb4886372699dcc10627b19
suchi <suchibankoti95@gmail.com>
2023-05-01 15:53:22 +0530

    display message on html

---
+++ test.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      const message = fs.readFileSync("msg.txt", "utf-8");
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<div>
+        <h2>${message}</h2>
+        <form action="/message" method="post">
+          <input type="text" name="message" />
+          <input type="submit" value="send" />
+        </form>
+      </div>`);
+      res.end();
+    }
+    if (req.url === "/message" && req.method === "POST") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body = chunk.toString();
+      });
+      req.on("end", () => {
+        const msg = body;
+        fs.writeFileSync("msg.txt", msg);
+      });
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
+    }
+  })
+  .listen(5000, () => {
+    console.log("server active");
+  });

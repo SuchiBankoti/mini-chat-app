45fcf67a37025f412823aecd8343d2eee45fd0d5
suchi <suchibankoti95@gmail.com>
2023-05-11 12:01:02 +0530

    delete data from mysql

--- test.js
+++ test.js
@@ -9,11 +9,20 @@ db.execute("SELECT * FROM chats")
   .then((data) => console.log(data[0]))
   .catch((err) => console.log(err));
 
-db.query(
-  'INSERT INTO chats(username, message) VALUES("kunal","new mesaage from kunal")'
-)
-  .then((data) => console.log("data inserted"))
+// db.query(
+//   'INSERT INTO chats(username, message) VALUES("kunal","new mesaage from kunal")'
+// )
+//   .then((data) => console.log("data inserted"))
+//   .catch((err) => console.log(err));
+
+db.query('UPDATE chats SET message="new updated message" WHERE id=2')
+  .then((data) => console.log("data updated"))
   .catch((err) => console.log(err));
+
+db.query('DELETE FROM chats  WHERE username="kunal"')
+  .then((data) => console.log("data deleted"))
+  .catch((err) => console.log(err));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(route);
 

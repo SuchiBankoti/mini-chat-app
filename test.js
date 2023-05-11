d4413b216da1e8ab57b93171544e82065d25060b
suchi <suchibankoti95@gmail.com>
2023-05-11 11:35:07 +0530

    insert data in mysql

--- test.js
+++ test.js
@@ -5,8 +5,15 @@ const path = require("path");
 const catchError = require("./controllers/error");
 const db = require("./util/database");
 
-db.execute("SELECT * FROM products");
+db.execute("SELECT * FROM chats")
+  .then((data) => console.log(data[0]))
+  .catch((err) => console.log(err));
 
+db.query(
+  'INSERT INTO chats(username, message) VALUES("kunal","new mesaage from kunal")'
+)
+  .then((data) => console.log("data inserted"))
+  .catch((err) => console.log(err));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(route);
 
